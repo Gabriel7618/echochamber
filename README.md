94e0d268e3577f6221d49a3478d0101ea8d08ca0
Gabriel7618 <gdcanimagine@gmail.com>
2025-04-14 16:58:45 +0100

    Setup Classifier
    Setup the classifier to be able to query a LLM.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # echochamber
 
-To use, first do `make all`. Then, `./echochamber`
+## Installation
+
+`classifier.py` uses [Groq](https://groq.com/) to query LLMs.
+
+Follow [this tutorial](https://console.groq.com/docs/quickstart) to get started, which gets you to:
+- Sign up for a (free) account
+- Create an API key
+- Configure your API key as an environment variable, by running
+  `export GROQ_API_KEY=<your-api-key-here>`
+  > **Warning:** Do **not** push any code or file containing your API key to a GitHub remote repo as this is a secret and must be kept secure!
+
+## Usage
+
+First do `make all`. Then, `./echochamber`
--- classifier.py
+++ classifier.py
@@ -1,5 +1,28 @@
-def classify(links):
-    result = []
-    for link in links:
-        result.append((link, 1))
-    return result
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+def classify(topic, title, body):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": (
+                        "Say Hello World!"
+                    ),
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+    print(chat_completion.choices[0].message.content)
+
+    return 0
+
+if __name__ == "__main__":
+    classify(None, None, None)
+
